260f164886b9ccc8915112c0efaeae7c4ece4f70
Brandon <handlez36@gmail.com>
2015-11-06 21:34:23 +0000

    Added authorization security and refactored lesson controller to clean the code up

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,22 +1,33 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
+  
   def new
-    @section = Section.find(params[:section_id])
-    @lesson = Lesson.new
+    @lesson = Lesson.new      
   end
   
   def create
-    @section = Section.find(params[:section_id])
-    @lesson = @section.lessons.create(lesson_params)
-    @course = @section.course
+    @lesson = current_section.lessons.create(lesson_params)
     
     if @lesson.valid?
-      redirect_to instructor_course_path(@course)
+      redirect_to instructor_course_path(current_section.course)
     else
       render :new, :status => :unprocessable_entity
     end
   end
   
   private
+  
+  def require_authorized_for_current_section
+    if current_user != current_section.course.user
+      return render :text => "Unauthorized User", :status => :unauthorized
+    end
+  end
+  
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
   
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
