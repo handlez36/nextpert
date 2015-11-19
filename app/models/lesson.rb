8fb4e56a2bfd54e8c5dbcbe25af6070c6eb8777d
Brandon <handlez36@gmail.com>
2015-11-19 11:17:42 +0000

    Added 'Next Lesson' button to the Lesson Show Page

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,21 @@ class Lesson < ActiveRecord::Base
   
   include RankedModel
   ranks :row_order, :with_same => :section_id
+  
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first    
+    
+    if lesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end
+    
+    return lesson
+  end
+  
+  def next_section
+    current_section = self.section
+    current_course = current_section.course
+    
+    section = current_course.sections.where("row_order > ?", current_section.row_order).rank(:row_order).first
+  end
 end
